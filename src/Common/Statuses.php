7bcba06cda27da0abbae4fc201ee20cdbeb84b26
David Sherlock <davidmichaelsherlock@icloud.com>
2024-11-21 13:57:19 +0300

    Refactoring

--- src/Common/Statuses.php
+++ src/Common/Statuses.php
@@ -18,13 +18,13 @@ class Statuses {
 	/**
 	 * Get the discount status options.
 	 *
-	 * @param bool $sort            Optional. Whether to sort alphabetically by label. Default true.
-	 * @param bool $include_expired Optional. Whether to include expired status. Default true.
+	 * This method retrieves an array of discount status options that can be used in various contexts
+	 * throughout the application. Each status is represented as an array with 'label' and 'value' keys.
 	 *
-	 * @return array Array of status options with label and value.
+	 * @return array Array of status options with label and value pairs.
 	 */
-	public static function get_discount_statuses( bool $sort = true, bool $include_expired = true ): array {
-		return [
+	public static function get_discount(): array {
+		$statuses = [
 			[
 				'label' => esc_html__( 'Active', 'arraypress' ),
 				'value' => 'active'
@@ -34,23 +34,33 @@ class Statuses {
 				'value' => 'inactive'
 			],
 			[
-				'label' => esc_html__( 'Archived', 'arraypress' ),
-				'value' => 'archived'
-			],
-			[
 				'label' => esc_html__( 'Expired', 'arraypress' ),
 				'value' => 'expired'
+			],
+			[
+				'label' => esc_html__( 'Archived', 'arraypress' ),
+				'value' => 'archived'
 			]
 		];
+
+		/**
+		 * Filters the array of discount status options.
+		 *
+		 * @param array $statuses The array of status options.
+		 */
+		return apply_filters( 'arraypress_discount_statuses', $statuses );
 	}
 
 	/**
 	 * Get the commission status options.
 	 *
-	 * @return array Array of status options with label and value.
+	 * This method retrieves an array of commission status options that can be used in various contexts
+	 * throughout the application. Each status is represented as an array with 'label' and 'value' keys.
+	 *
+	 * @return array Array of status options with label and value pairs.
 	 */
-	public static function get_commission_statuses(): array {
-		return [
+	public static function get_commission(): array {
+		$statuses = [
 			[
 				'label' => esc_html__( 'Unpaid', 'arraypress' ),
 				'value' => 'unpaid'
@@ -64,6 +74,51 @@ class Statuses {
 				'value' => 'revoked'
 			]
 		];
+
+		/**
+		 * Filters the array of commission status options.
+		 *
+		 * @param array $statuses The array of status options.
+		 */
+		return apply_filters( 'arraypress_commission_statuses', $statuses );
+	}
+
+	/**
+	 * Get the customer status options.
+	 *
+	 * This method retrieves an array of customer status options that can be used in various contexts
+	 * throughout the application. Each status is represented as an array with 'label' and 'value' keys.
+	 *
+	 * @return array Array of status options with label and value pairs.
+	 * @since 1.0.0
+	 *
+	 */
+	public static function get_customer(): array {
+		$statuses = [
+			[
+				'label' => esc_html__( 'Active', 'arraypress' ),
+				'value' => 'active'
+			],
+			[
+				'label' => esc_html__( 'Inactive', 'arraypress' ),
+				'value' => 'inactive'
+			],
+			[
+				'label' => esc_html__( 'Disabled', 'arraypress' ),
+				'value' => 'disabled'
+			],
+			[
+				'label' => esc_html__( 'Pending', 'arraypress' ),
+				'value' => 'pending'
+			]
+		];
+
+		/**
+		 * Filters the array of customer status options.
+		 *
+		 * @param array $statuses The array of status options.
+		 */
+		return apply_filters( 'arraypress_customer_statuses', $statuses );
 	}
 
 }
