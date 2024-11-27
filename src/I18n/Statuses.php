0023a530d0aea9f2e7f1e288fef3ed64230394b3
David Sherlock <davidmichaelsherlock@icloud.com>
2024-11-28 00:51:01 +0300

    Refactoring

--- src/I18n/Statuses.php
+++ src/I18n/Statuses.php
@@ -11,7 +11,7 @@
 
 declare( strict_types=1 );
 
-namespace ArrayPress\EDD\Common;
+namespace ArrayPress\EDD\I18n;
 
 class Statuses {
 
@@ -119,6 +119,74 @@ class Statuses {
 		 * @param array $statuses The array of status options.
 		 */
 		return apply_filters( 'arraypress_customer_statuses', $statuses );
+	}
+
+	/**
+	 * Get the date period options.
+	 *
+	 * This method retrieves an array of date period options that can be used in various contexts
+	 * throughout the application. Each period is represented as an array with 'label' and 'value' keys.
+	 *
+	 * @return array Array of period options with label and value pairs.
+	 */
+	public static function get_date_periods(): array {
+		$periods = [
+			[
+				'label' => esc_html__( 'All Time', 'arraypress' ),
+				'value' => 'all_time'
+			],
+			[
+				'label' => esc_html__( 'Today', 'arraypress' ),
+				'value' => 'today'
+			],
+			[
+				'label' => esc_html__( 'Yesterday', 'arraypress' ),
+				'value' => 'yesterday'
+			],
+			[
+				'label' => esc_html__( 'This Week', 'arraypress' ),
+				'value' => 'this_week'
+			],
+			[
+				'label' => esc_html__( 'Last Week', 'arraypress' ),
+				'value' => 'last_week'
+			],
+			[
+				'label' => esc_html__( 'Last 30 Days', 'arraypress' ),
+				'value' => 'last_30_days'
+			],
+			[
+				'label' => esc_html__( 'This Month', 'arraypress' ),
+				'value' => 'this_month'
+			],
+			[
+				'label' => esc_html__( 'Last Month', 'arraypress' ),
+				'value' => 'last_month'
+			],
+			[
+				'label' => esc_html__( 'This Quarter', 'arraypress' ),
+				'value' => 'this_quarter'
+			],
+			[
+				'label' => esc_html__( 'Last Quarter', 'arraypress' ),
+				'value' => 'last_quarter'
+			],
+			[
+				'label' => esc_html__( 'This Year', 'arraypress' ),
+				'value' => 'this_year'
+			],
+			[
+				'label' => esc_html__( 'Last Year', 'arraypress' ),
+				'value' => 'last_year'
+			]
+		];
+
+		/**
+		 * Filters the array of date period options.
+		 *
+		 * @param array $periods The array of period options.
+		 */
+		return apply_filters( 'arraypress_date_periods', $periods );
 	}
 
 }
