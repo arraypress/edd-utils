d489a38a1a393228429ca264ce0a6b97a2638df2
David Sherlock <davidmichaelsherlock@icloud.com>
2024-12-02 00:48:22 +0300

    Refactoring

--- src/Traits/Download/Earnings.php
+++ src/Traits/Download/Earnings.php
@@ -19,6 +19,13 @@ use EDD\Stats;
 trait Earnings {
 
 	/**
+	 * Meta key for storing earnings stats
+	 *
+	 * @var string
+	 */
+	protected static string $earnings_meta_key = 'edd_product_earnings_stats';
+
+	/**
 	 * Get earnings for a product.
 	 *
 	 * @param int         $product_id The product ID
@@ -72,6 +79,100 @@ trait Earnings {
 		$args['revenue_type'] = 'net';
 
 		return self::get_earnings( $product_id, $price_id, $period, $args );
+	}
+
+	/**
+	 * Process and cache all period stats for a product.
+	 *
+	 * @param int      $product_id The product ID
+	 * @param int|null $price_id   Optional. The price ID. Default null.
+	 * @param array    $args       Optional. Additional query arguments.
+	 *
+	 * @return array Array of stats for all periods.
+	 */
+	public static function process_stats( int $product_id, ?int $price_id = null, array $args = [] ): array {
+		if ( empty( $product_id ) ) {
+			return [];
+		}
+
+		$meta_key = self::$earnings_meta_key;
+		if ( ! is_null( $price_id ) ) {
+			$meta_key .= '_' . $price_id;
+		}
+
+		$stats = array();
+
+		// Get all available periods
+		foreach ( Dates::get_periods() as $period ) {
+			$stats[ $period ] = array(
+				'earnings'               => self::get_earnings( $product_id, $price_id, $period, $args ),
+				'net_earnings'           => self::get_net_earnings( $product_id, $price_id, $period, $args ),
+				'average_daily_earnings' => self::get_average_daily_earnings( $product_id, $price_id, $period, $args ),
+				'generated'              => time()
+			);
+		}
+
+		// Store the stats in post meta
+		update_post_meta( $product_id, $meta_key, $stats );
+
+		return $stats;
+	}
+
+	/**
+	 * Get cached stats for a product.
+	 *
+	 * @param int      $product_id The product ID
+	 * @param int|null $price_id   Optional. The price ID. Default null.
+	 * @param string   $period     Optional. Specific period to retrieve. Default returns all periods.
+	 * @param bool     $force      Optional. Force regeneration of stats. Default false.
+	 *
+	 * @return array Array of cached stats
+	 */
+	public static function get_cached_stats( int $product_id, ?int $price_id = null, ?string $period = null, bool $force = false ): array {
+		if ( empty( $product_id ) ) {
+			return [];
+		}
+
+		$meta_key = self::$earnings_meta_key;
+		if ( ! is_null( $price_id ) ) {
+			$meta_key .= '_' . $price_id;
+		}
+
+		// Get cached stats
+		$stats = get_post_meta( $product_id, $meta_key, true );
+
+		// If no stats exist, force is true, or stats are older than 24 hours, regenerate them
+		if ( empty( $stats ) || $force || ! isset( $stats['all_time']['generated'] ) || ( time() - $stats['all_time']['generated'] ) > DAY_IN_SECONDS ) {
+			$stats = self::process_stats( $product_id, $price_id );
+		}
+
+		// Return specific period if requested
+		if ( ! is_null( $period ) && isset( $stats[ $period ] ) ) {
+			return $stats[ $period ];
+		}
+
+		return $stats;
+	}
+
+	/**
+	 * Clear cached stats for a product.
+	 *
+	 * @param int      $product_id The product ID
+	 * @param int|null $price_id   Optional. The price ID. Default null.
+	 *
+	 * @return bool Whether the meta was successfully deleted.
+	 */
+	public static function clear_cached_stats( int $product_id, ?int $price_id = null ): bool {
+		if ( empty( $product_id ) ) {
+			return false;
+		}
+
+		$meta_key = self::$earnings_meta_key;
+		if ( ! is_null( $price_id ) ) {
+			$meta_key .= '_' . $price_id;
+		}
+
+		return delete_post_meta( $product_id, $meta_key );
 	}
 
 }
